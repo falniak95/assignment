fa963c42f46af6f89370b7421db058c59c064d65
Furkan ALNIAK <info@furkanalniak.com>
2025-01-30 17:29:16 +0300

    Project init with default dependencies

---
+++ src/test/java/com/furkanalniak/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furkanalniak.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
