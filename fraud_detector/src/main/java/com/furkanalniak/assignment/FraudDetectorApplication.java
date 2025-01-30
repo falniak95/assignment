f4b3fd30c6bbe9a3f984f6cb0fb7faa67ea01f61
Furkan ALNIAK <info@furkanalniak.com>
2025-01-30 18:12:59 +0300

    Add microservices: transaction_producer and fraud_detector

---
+++ fraud_detector/src/main/java/com/furkanalniak/assignment/FraudDetectorApplication.java
@@ -0,0 +1,11 @@
+package com.furkanalniak.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudDetectorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FraudDetectorApplication.class, args);
+    }
+} 
---
+++ transaction_producer/src/main/java/com/furkanalniak/assignment/TransactionProducerApplication.java
@@ -0,0 +1,11 @@
+package com.furkanalniak.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionProducerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionProducerApplication.class, args);
+    }
+} 
